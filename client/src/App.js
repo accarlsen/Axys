cb70c20dfbfcdf73b118585145122ba82750e28d
Alexander <alexac@student.ntnu.no>
2021-12-09 18:40:43 +0100

    Implemented prototype of customization of root variables

--- client/src/App.js
+++ client/src/App.js
@@ -11,12 +11,39 @@ import TaskList from './pages/TaskList/taskList';
 import Network from './pages/network/network';
 import Profile from './pages/profile/profile';
 import AssignmentList from './pages/TaskList/assignmentList';
+import Customize from './pages/customize/customize';
 
 function App() {
   const [token, setToken] = useState(null);
   const [personId, setPersonId] = useState(null);
   const [tokenExpiration, setTokenExpiration] = useState(null);
   const [admin, setAdmin] = useState(null);
+  const [theme, setTheme] = useState("darkTheme");
+
+  const lightTheme = {
+    "--background": "white",
+    "--background2": "grey",
+    "--background3": "white",
+  };
+  const darkTheme = {
+    "--background": "#1b1b1b",
+    "--background2": "#222",
+    "--background3": "#1d1d1d",
+  };
+
+  const toggleTheme = () => {
+    if (theme === "dark") setTheme("light")
+    else setTheme("dark")
+    applyTheme(theme)
+  }
+
+  const applyTheme = theme => {
+    const theTheme = theme === "dark" ? lightTheme : darkTheme;
+    Object.keys(theTheme).map(key => {
+      const value = theTheme[key];
+      document.documentElement.style.setProperty(key, value);
+    });
+  };
 
 
   return (
@@ -42,6 +69,10 @@ function App() {
           <Route path="/profile/">
             <NavBar />
             <Profile />
+          </Route>
+          <Route path="/customization">
+            <NavBar />
+            <Customize theme={theme} toggleTheme={toggleTheme} />
           </Route>
           <Route path="/">
             <NavBar />
---
+++ client/src/pages/customize/customize.js
@@ -0,0 +1,42 @@
+import { useMutation, useQuery } from '@apollo/client';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { editProfile, getProfile } from '../../components/queries';
+
+import style from './customize.module.css';
+
+function Customize(props) {
+    const id = localStorage.getItem("personId")
+
+    const { loading, error, data } = useQuery(getProfile, {
+        variables: { id: id }
+    });
+
+    if (data) return (<CustomizationRenderer profile={data} id={id} toggleTheme={props.toggleTheme} />)
+    return (<div></div>)
+}
+
+function CustomizationRenderer(props) {
+    const data = props.profile
+    const id = props.id
+
+    //Render
+    return (
+        <div className={style.wrapper}>
+            <div className={style.cardWrapper}>
+
+                <div className={style.card}>
+                    <div className={style.editWrapper}>
+                        <div className={style.editFormGrid}>
+                            <label className="p">First Name</label>
+                            <input className="input" ></input>
+                        </div>
+                        <button className="button" onClick={()=> props.toggleTheme()}>Toggle theme</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Customize;
